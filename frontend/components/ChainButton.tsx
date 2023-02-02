9cdb6aa8ed055bdf280e95b6f9e9b2a0aa6c2e16
牛叠谊 <18715156450@163.com>
2023-02-02 19:18:51 +0800

    chore: image tiny

--- frontend/components/ChainButton.tsx
+++ frontend/components/ChainButton.tsx
@@ -13,6 +13,7 @@ export function ChainButton(props: IChainButtonProps) {
       href={link}
       target="_blank"
       className="items-center justify-center flex px-3 py-[4px] xl:px-4 xl:py-[6px] gap-x-[7px] xl:gap-x-[11px] rounded-lg shadow-[inset_0px_0px_1px_rgba(0,0,0,0.25)] bg-secondaryBlack"
+      rel="noreferrer"
     >
       <Image src={icon} alt={name} height={18} />
       <span className="text-sm xl:text-base">{name}</span>
